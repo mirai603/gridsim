5fcc6fec29aedeaebd6f8372b3318144853b4e3a
zihan0603 <148586394+zihan0603@users.noreply.github.com>
2024-08-30 19:42:18 -0400

    Working button toaster, working grid logger

--- app/src/main/java/zihan/gridsim/MainActivity.java
+++ app/src/main/java/zihan/gridsim/MainActivity.java
@@ -1,8 +1,14 @@
 package zihan.gridsim;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ListAdapter;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,5 +31,29 @@ public class MainActivity extends AppCompatActivity {
 
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                //test toast
+                //Toast.makeText(v.getContext(), Integer.toString(position), Toast.LENGTH_LONG).show();
+                Log.d("gridView", Integer.toString(position));
+            }
+        });
+    }
+
+
+    public void button1(View v) {
+        Toast.makeText(this, "I'm Button 1", Toast.LENGTH_LONG).show();
+    }
+
+    public void button2(View v) {
+        Toast.makeText(this, "I'm Button 2", Toast.LENGTH_LONG).show();
+    }
+
+    public void logtest(View v, int pos) {
+        Toast.makeText(this, pos, Toast.LENGTH_LONG).show();
     }
 }
